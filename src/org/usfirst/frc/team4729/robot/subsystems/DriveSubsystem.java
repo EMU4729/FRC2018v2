0bf632351a1927d655b4f9654e06d95c479e3a10
EMU4729 <unswfrcmedia@gmail.com>
2018-02-12 18:09:39 +1100

    Changed to PWM

--- src/org/usfirst/frc/team4729/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team4729/robot/subsystems/DriveSubsystem.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team4729.robot.subsystems;
 import org.usfirst.frc.team4729.robot.Robot;
 import org.usfirst.frc.team4729.robot.RobotMap;
 import edu.wpi.first.wpilibj.PWMSpeedController;
-
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,10 +15,10 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
  *
  */
 public class DriveSubsystem extends Subsystem {
-    TalonSRX leftFrontDrive;
-    TalonSRX leftBackDrive;
-    TalonSRX rightFrontDrive;
-    TalonSRX rightBackDrive;
+    Talon leftFrontDrive;
+    Talon leftBackDrive;
+    Talon rightFrontDrive;
+    Talon rightBackDrive;
     
     Encoder leftEncoder;
     Encoder rightEncoder;
@@ -42,10 +42,10 @@ public class DriveSubsystem extends Subsystem {
     // here. Call these from Commands.
     
     public DriveSubsystem() {
-    	leftFrontDrive = new TalonSRX(RobotMap.MOTOR_LEFT_FRONT);
-    	leftBackDrive = new TalonSRX(RobotMap.MOTOR_LEFT_BACK);
-    	rightFrontDrive = new TalonSRX(RobotMap.MOTOR_RIGHT_FRONT);
-    	rightBackDrive = new TalonSRX(RobotMap.MOTOR_RIGHT_BACK);
+    	leftFrontDrive = new Talon(RobotMap.MOTOR_LEFT_FRONT);
+    	leftBackDrive = new Talon(RobotMap.MOTOR_LEFT_BACK);
+    	rightFrontDrive = new Talon(RobotMap.MOTOR_RIGHT_FRONT);
+    	rightBackDrive = new Talon(RobotMap.MOTOR_RIGHT_BACK);
     	
 //    	leftEncoder = new Encoder(RobotMap.ENCODER_LEFT_A, RobotMap.ENCODER_LEFT_B, En);
     	leftEncoder = new Encoder(RobotMap.ENCODER_LEFT_A, RobotMap.ENCODER_LEFT_B, false, Encoder.EncodingType.k4X);

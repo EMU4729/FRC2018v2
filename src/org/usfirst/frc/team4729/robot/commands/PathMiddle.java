7af30179c762b3ffe5db2825d2fbfbf04922ea98
B3NJP <b3np@pearcy.org>
2018-02-16 17:12:51 +1100

    Added Autos

--- src/org/usfirst/frc/team4729/robot/commands/PathMiddle.java
+++ src/org/usfirst/frc/team4729/robot/commands/PathMiddle.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4729.robot.commands;
 
+import org.usfirst.frc.team4729.robot.enums.Direction;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
@@ -7,22 +9,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class PathMiddle extends CommandGroup {
 
-    public PathMiddle() {
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
+    public PathMiddle(Direction side) {
+    		switch (side) {
+    		case LEFT:
+    			addSequential(new PathMiddleLeft());
+    			break;
+    		case RIGHT:
+    			addSequential(new PathMiddleRight());
+    			break;
+    		}
     }
 }
